4261d8c823b3aff5e9e81f87a28e891a39a0c261
Alexahder Kochish <shvepsolek@gmail.com>
2025-04-27 18:15:38 +0100

    add lazy loading and spinner

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,19 +1,33 @@
+import { lazy, Suspense } from "react";
 import { createBrowserRouter } from "react-router-dom";
-import SignUp from "../pages/auth/sign-up/SignUp";
-import SignIn from "../pages/auth/sign-in/SignIn";
-import Chat from "../pages/chats/Chat";
+const SignUp = lazy(() => import("../pages/auth/sign-up/SignUp"));
+const SignIn = lazy(() => import("../pages/auth/sign-in/SignIn"));
+const Chat = lazy(() => import("../pages/chats/Chat"));
+import Spinner from "../components/ui/Spinner/Spinner";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Chat />,
+    element: (
+      <Suspense fallback={<Spinner />}>
+        <Chat />
+      </Suspense>
+    ),
   },
   {
     path: "/sign-up",
-    element: <SignUp />,
+    element: (
+      <Suspense fallback={<Spinner />}>
+        <SignUp />
+      </Suspense>
+    ),
   },
   {
     path: "/sign-in",
-    element: <SignIn />,
+    element: (
+      <Suspense fallback={<Spinner />}>
+        <SignIn />
+      </Suspense>
+    ),
   },
 ]);
